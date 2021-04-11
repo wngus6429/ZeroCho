7c4ec7827449eac668611d7fe13b74d88ffc4cfd
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2021-04-12 02:44:53 +0900

    04

--- 자바스크립트/1번째 끝말잇기/dictionary.js
+++ 자바스크립트/1번째 끝말잇기/dictionary.js
@@ -1,15 +1,19 @@
 /** @format */
 
 //input 만 value고 div, span, button은 다 textContent 대소문자 주의
-const button = document.querySelector("#button");
+const btn = document.querySelector("#button");
 
-button.addEventListener("click", () => {
+const dictionary = [];
+btn.addEventListener("click", () => {
   const wordTag = document.querySelector("#word");
   const word = wordTag.textContent; //값 가져옴
   const inputTag = document.querySelector("#input");
   const errorTag = document.querySelector("#error");
   const input = inputTag.value; //값 가져옴
-  if (word[word.length - 1] === input[0]) {
+  if (dictionary.includes(input)) {
+    errorTag.textContent = "중복입니다";
+  } else {
+    if (word[word.length - 1] === input[0]) {
       wordTag.textContent = input;
       errorTag.textContent = "";
       inputTag.value = "";

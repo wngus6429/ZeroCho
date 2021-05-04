f2e0c6d83df7e58a371746d283fc2deb324b35ff
ParkJuhyun <wngus6429@gmail.com>
2021-05-04 18:47:38 +0900

    webpack3
    webpack3

--- React/webpack/WordRelay.jsx
+++ React/webpack/WordRelay.jsx
@@ -1,15 +1,46 @@
 const React = require("react");
-const { Component } = React;
+const { useState, useRef } = React;
 
 // class WordRelay extends React.Component {
-class WordRelay extends Component {
-  state = {
-    text: "Hello, webpack",
+const WordRelay = () => {
+  const [word, setWord] = useState("주현님");
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+  const inputRef = useRef(null);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+      setResult("딩동댕");
+      setWord(value);
+      setValue("");
+      inputRef.current.focus();
+    } else {
+      setResult("땡");
+      setValue("");
+      inputRef.current.focus();
+    }
   };
-  render() {
-    return <h1>{this.state.text}</h1>;
-  }
-}
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <label id="label" htmlFor="wordinput">
+          글자를 입력하세요
+        </label>
+        <br />
+        <input id="wordinput" className="wordinput" ref={inputRef} value={value} onChange={onChangeInput} />
+        <button>입력</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
+};
 
 module.exports = WordRelay;
 //쪼갠 파일을 밖에서도 사용할수 있게. 노드의 모듈시스템
--- React/webpack/webpack.config.js
+++ React/webpack/webpack.config.js
@@ -1,38 +1,38 @@
-const path = require("path"); //걍 외워
-//노드에서 경로 쉽게 조작하도록 하는 기능
+const path = require("path");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
-  name: "park-wordrelay", //웹팩설정에 대한 이름 설정인데 마음대로 가능
-  mode: "development", //실서비스에서는 production
-  devtool: "eval", //빠르게 하겠다는거, 개발:eval, 배포:hidden-source-map
+  name: "park-relay",
+  mode: "development",
+  devtool: "eval", //배포는 hidden-source-map
   resolve: {
-    extensions: [".js", ".jsx"], //알아서 웹팩이 확장자 찾음 밑에 entry에서 확장자 꼭 붙일 필요 없음.
+    extensions: [".js", ".jsx"],
   },
   entry: {
-    app: ["./client"],
-  }, //입력, 확장자도 안 넣어도됨
-  //app: ["./client.jsx", "./WordRelay.jsx"], //사실 이게 맞는데 웹팩이 알아서 판단함
-  //client.jsx안에 wordRelay파일을 불러오는게 보이거든, 물론 위에 react, reactDom도 같이 불러옴
-
-  //entry파일들을 module 적용해서 output으로 뺀다
+    app: "./client", //위에 resolve로 확장자 생략
+  },
   module: {
     rules: [
       {
-        test: /\.jsx?/, //js파일이랑 jsx을 이 룰을 적용하겠다
-        loader: "babel-loader", //js랑jsx파일에 바벨을 적용해서 최신아니 실험적 문법을 옛날문법으로 바꿔주겟다.
+        test: /\.jsx?$/,
+        loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-          plugins: ["@babel/plugin-proposal-class-properties"],
-        }, //바벨옵션, 이러면 js, jsx을 바벨 적용 해줄거임
-      },
-    ],
+          presets: [["@babel/preset-env", { targets: { browsers: ["> 1% in KR"] }, debug: true }], "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties", "react-refresh/babel"],
+        }, //targets적은게 앞preset-env의 설정한거임. 브라우저 마지막버전의 2단계 앞까지만 지원
+      }, //옛날거로 갈수록 최신문법 지원 해주는게 어려운게 어려우니, 저걸 해주면 바벨의 작업양도 적어지니 빨라짐
+    ], // presets애가 플러그인들 모음임 , browserlist 홈페이지 확인하기
   },
-
+  //plugins: [new webpack.LoaderOptionsPlugin({ debug: true })], //확장 프로그램 같은 느낌
+  //현업에서 보면 plugins많다. 다 지워서 하나하나 확인해보는게 좋음
+  plugins: [new ReactRefreshWebpackPlugin()],
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "app.js", // 원하는 파일이름
-  }, //출력 , path.join 경로를 알아서 합쳐줌 , __dirname은 현재폴더
-  //컴퓨터 마다 C드라이버안에 어디어디 졸라 복잡하니 간단해결
+    filename: "app.js",
+    publicPath: "/dist/",
+  },
+  devServer: {
+    publicPath: "/dist/",
+    hot: true,
+  },
 };
-
-//en

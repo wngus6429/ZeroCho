27cc42bcf19a6a807e1c2018c3341d3ce4bb5291
ParkJuhyun <wngus6429@gmail.com>
2022-02-27 19:12:16 +0900

    Update FollowList.vue

--- vue-nordbird/ch1/front/components/FollowList.vue
+++ vue-nordbird/ch1/front/components/FollowList.vue
@@ -1,9 +1,18 @@
 <template>
   <v-list>
-    <v-list-item v-for="user in propdata" :key="user.id">
-      <span>{{ user.nickname }}</span>
-      <v-icon @click="deletefunc(user.id)">mdi-minus-circle-outline</v-icon>
-    </v-list-item>
+    <v-col v-for="user in propdata" :key="user.id" cols="12" md="4" style="display: inline-block">
+      <v-list-item>
+        <v-list-item-avatar color="indigo">
+          <span class="white--text headline">{{ user.nickname[0] }}</span>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>{{ user.nickname }}</v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-icon @click="remove(user.id)">mdi-minus-circle-outline</v-icon>
+        </v-list-item-action>
+      </v-list-item>
+    </v-col>
   </v-list>
 </template>
 

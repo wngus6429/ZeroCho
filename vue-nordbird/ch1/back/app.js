c321f89fdc3b5ccb0c69351665acd8fa600ea9e3
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2022-03-08 02:12:02 +0900

    update back 0308

---
+++ vue-nordbird/ch1/back/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+// 앞에 localhost:3000 숨겨짐
+app.get("/", (req, res) => {
+  res.send("안녕 백엔드");
+  //   res.status(200).send('안녕백엔드')
+});
+
+app.listen(3000, () => {
+  console.log(`백엔드서버${3000}번 포트에서 작동중`);
+});

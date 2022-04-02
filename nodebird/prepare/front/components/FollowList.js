de9b6c6b046d113607dd3160a3ed9753d9ed5ed9
ParkJuhyun <wngus6429@gmail.com>
2022-04-02 18:24:14 +0900

    update 04/02

--- nodebird/prepare/front/components/FollowList.js
+++ nodebird/prepare/front/components/FollowList.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { Button, Card, List } from "antd";
-import Proptypes from "prop-types";
-import { StopTwoTone } from "@ant-design/icons";
-import { useDispatch } from "react-redux";
-import { UNFOLLOW_REQUEST, REMOVE_FOLLOWER_REQUEST } from "../reducers/user";
+import React from 'react';
+import { Button, Card, List } from 'antd';
+import Proptypes from 'prop-types';
+import { StopTwoTone } from '@ant-design/icons';
+import { useDispatch } from 'react-redux';
+import { UNFOLLOW_REQUEST, REMOVE_FOLLOWER_REQUEST } from '../reducers/user';
 
 const FollowList = ({ header, data, onClickMore, loading }) => {
   const dispatch = useDispatch();
   const onCancel = (id) => () => {
-    if (header === "팔로잉") {
+    if (header === '팔로잉') {
       dispatch({
         type: UNFOLLOW_REQUEST,
         data: id,
@@ -25,10 +25,10 @@ const FollowList = ({ header, data, onClickMore, loading }) => {
     <List
       style={{ marginBottom: 20 }}
       grid={{ gutter: 4, xs: 2, md: 3 }}
-      size="small"
+      size='small'
       header={<div>{header}</div>}
       loadMore={
-        <div style={{ textAlign: "center", margin: "10px 0" }}>
+        <div style={{ textAlign: 'center', margin: '10px 0' }}>
           <Button onClick={onClickMore} loading={loading}>
             더 보기
           </Button>
@@ -38,7 +38,7 @@ const FollowList = ({ header, data, onClickMore, loading }) => {
       dataSource={data}
       renderItem={(item) => (
         <List.Item style={{ marginTop: 20 }}>
-          <Card actions={[<StopTwoTone key="stop" onClick={onCancel(item.id)} />]}>
+          <Card actions={[<StopTwoTone key='stop' onClick={onCancel(item.id)} />]}>
             <Card.Meta description={item.nickname} />
           </Card>
         </List.Item>
--- nodebird/prepare/front/components/LoginForm.js
+++ nodebird/prepare/front/components/LoginForm.js
@@ -1,10 +1,10 @@
-import React, { useCallback, useEffect } from "react";
-import { Form, Input, Button } from "antd";
-import Link from "next/link";
-import styled from "styled-components";
-import useInput from "../hooks/useinput";
-import { useDispatch, useSelector } from "react-redux";
-import { loginRequestAction } from "../reducers/user";
+import React, { useCallback, useEffect } from 'react';
+import { Form, Input, Button } from 'antd';
+import Link from 'next/link';
+import styled from 'styled-components';
+import useInput from '../hooks/useinput';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginRequestAction } from '../reducers/user';
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
@@ -26,8 +26,8 @@ const FormWrapper = styled(Form)`
 const LoginForm = () => {
   const dispatch = useDispatch();
   const { logInLoading, logInError } = useSelector((state) => state.user);
-  const [email, onChangeEmail] = useInput("");
-  const [password, onChangePassword] = useInput("");
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
 
   useEffect(() => {
     if (logInError) {
@@ -45,22 +45,22 @@ const LoginForm = () => {
   return (
     <FormWrapper onFinish={onSubmitForm}>
       <div>
-        <label htmlFor="user-email">이메일ㅗㅗ</label>
+        <label htmlFor='user-email'>이메일</label>
         <br />
-        <Input name="user-email" type="email" value={email} onChange={onChangeEmail} required />
+        <Input name='user-email' type='email' value={email} onChange={onChangeEmail} required />
       </div>
       <div>
-        <label htmlFor="user-password">비밀번호</label>
+        <label htmlFor='user-password'>비밀번호</label>
         <br />
-        <Input name="user-password" type="password" value={password} onChange={onChangePassword} required />
+        <Input name='user-password' type='password' value={password} onChange={onChangePassword} required />
       </div>
       <ButtonWrapper>
-        <Button type="primary" htmlType="submit" loading={logInLoading}>
+        <Button type='primary' htmlType='submit' loading={logInLoading}>
           로그인
         </Button>
-        <Link href="/signup">
+        <Link href='/signup'>
           <a>
-            <Button type="danger">회원가입</Button>
+            <Button type='danger'>회원가입</Button>
           </a>
         </Link>
       </ButtonWrapper>

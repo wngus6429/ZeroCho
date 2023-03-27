afda33e8cd9942cb594bf09280f6aace5e143f9d
Park Juhyun <wngus6429@gmail.com>
2023-03-27 17:17:55 +0900

    Delete 게시판 포트폴리오 버전별 모음 directory

--- 게시판 포트폴리오 버전별 모음/게시판포트폴리오7 완성/front/hooks/useinput.js
+++
@@ -1,9 +0,0 @@
-import { useState, useCallback } from "react";
-
-export default (initialValue = null) => {
-  const [Value, setValue] = useState(initialValue);
-  const handler = useCallback((e) => {
-    setValue(e.target.value);
-  }, []);
-  return [Value, handler, setValue];
-};

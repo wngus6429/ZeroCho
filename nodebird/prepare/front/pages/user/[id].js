3a5a5c043afd2ce337e5108b87f969debbce9a8c
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2023-04-25 19:34:55 +0900

    Update [id].js

--- nodebird/prepare/front/pages/user/[id].js
+++ nodebird/prepare/front/pages/user/[id].js
@@ -1,17 +1,17 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Avatar, Card } from "antd";
-import { END } from "redux-saga";
-import Head from "next/head";
-import { useRouter } from "next/router";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Avatar, Card } from 'antd';
+import { END } from 'redux-saga';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 //특정 사용자에 대한 게스글을 가져온다
-import axios from "axios";
-import { LOAD_USER_POSTS_REQUEST } from "../../reducers/post";
+import axios from 'axios';
+import { LOAD_USER_POSTS_REQUEST } from '../../reducers/post';
 //Loadpostrequest와 같은데. 이건 특정 사용자의 글만 보여주기 위해
-import { LOAD_MY_INFO_REQUEST, LOAD_USER_REQUEST } from "../../reducers/user";
-import PostCard from "../../components/PostCard";
-import wrapper from "../../store/configureStore";
-import AppLayout from "../../components/AppLayout";
+import { LOAD_MY_INFO_REQUEST, LOAD_USER_REQUEST } from '../../reducers/user';
+import PostCard from '../../components/PostCard';
+import wrapper from '../../store/configureStore';
+import AppLayout from '../../components/AppLayout';
 
 const User = () => {
   const dispatch = useDispatch();
@@ -22,10 +22,7 @@ const User = () => {
 
   useEffect(() => {
     const onScroll = () => {
-      if (
-        window.pageYOffset + document.documentElement.clientHeight >
-        document.documentElement.scrollHeight - 300
-      ) {
+      if (window.pageYOffset + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
         if (hasMorePosts && !loadPostsLoading) {
           dispatch({
             type: LOAD_USER_POSTS_REQUEST,
@@ -35,9 +32,9 @@ const User = () => {
         }
       }
     };
-    window.addEventListener("scroll", onScroll);
+    window.addEventListener('scroll', onScroll);
     return () => {
-      window.removeEventListener("scroll", onScroll);
+      window.removeEventListener('scroll', onScroll);
     };
   }, [mainPosts.length, hasMorePosts, id, loadPostsLoading]);
 
@@ -49,28 +46,28 @@ const User = () => {
             {userInfo.nickname}
             님의 글
           </title>
-          <meta name="description" content={`${userInfo.nickname}님의 게시글`} />
-          <meta property="og:title" content={`${userInfo.nickname}님의 게시글`} />
-          <meta property="og:description" content={`${userInfo.nickname}님의 게시글`} />
-          <meta property="og:image" content="https://nodebird.com/favicon.ico" />
-          <meta property="og:url" content={`https://nodebird.com/user/${id}`} />
+          <meta name='description' content={`${userInfo.nickname}님의 게시글`} />
+          <meta property='og:title' content={`${userInfo.nickname}님의 게시글`} />
+          <meta property='og:description' content={`${userInfo.nickname}님의 게시글`} />
+          <meta property='og:image' content='https://nodebird.com/favicon.ico' />
+          <meta property='og:url' content={`https://nodebird.com/user/${id}`} />
         </Head>
       )}
       {userInfo && userInfo.id !== me?.id ? (
         <Card
           style={{ marginBottom: 20 }}
           actions={[
-            <div key="twit">
+            <div key='twit'>
               짹짹
               <br />
               {userInfo.Posts}
             </div>,
-            <div key="following">
+            <div key='following'>
               팔로잉
               <br />
               {userInfo.Followings}
             </div>,
-            <div key="follower">
+            <div key='follower'>
               팔로워
               <br />
               {userInfo.Followers}

c3b7ae3957a59434c305979a4fc10d0e5d25f96c
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2023-12-12 03:19:39 +0900

    Update first.ts

--- typescript/first.ts
+++ typescript/first.ts
@@ -1,2 +1,11 @@
 let a = 'hello';
 a = 444;
+const a: number = 5;
+// function add(x: number, y: number): number {
+//   return x + y;
+// }
+// const add: (x: number, y: number) => number = (x, y) => x + y;
+type Add = (x: number, y: number) => number;
+const add: Add = (x, y) => x + y;
+
+const obj: { lat: number; lon: number } = { lat: 37.5, lon: 127.5 };

ef5168596870eb942caef54dc0e6fa19e13032f3
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2023-12-21 03:50:14 +0900

    Update

--- typescript/first.ts
+++ typescript/first.ts
@@ -9,3 +9,26 @@ type Add = (x: number, y: number) => number;
 const add: Add = (x, y) => x + y;
 
 const obj: { lat: number; lon: number } = { lat: 37.5, lon: 127.5 };
+
+try {
+  const array: string[] = [];
+  array.push('hello');
+} catch (error) {
+  error;
+}
+
+// const head:Element = document.querySelector('#head')!;
+// console.log(head);
+
+// <div id="header"></div>
+// !의 역할은 head에 추천 null이 사라진다. 무조건 존재한다는뜻 나중에 사라짐
+//* 근데 비추천임. !를 쓰는것보다 if 조건문을 아래에 쓰자
+// const head = document.querySelector('#head')!;
+const head = document.querySelector('#head');
+// head.innerHTML = 'hello'; //이렇게 하면 에러남. null나올수도 있다고
+if (head) {
+  head.innerHTML = 'Hello';
+}
+//! 소문자 string 써야함, 대문자 String쓰면 지옥임.
+const e: string = 'hello';
+const f: String = 'hell';
---
+++ typescript/first2.ts
@@ -0,0 +1,25 @@
+type World = 'world' | 'hell';
+const a: World = 'world';
+
+const b = `hello ${a}`;
+// type Greeting = 'hello world';
+// 타입에서도 템플릿 리터럴 (백틱) 사용이 가능하다.
+type Greeting = `hello ${World}`;
+const c: Greeting = 'hello hell';
+
+let arr: string[] = [];
+let arr2: Array<string> = [];
+function rest(...args: string[]) {
+  console.log(args); // [1,2,3]
+}
+rest('1', '2', '3');
+
+function rest2(a, ...args: string[]) {
+  console.log(a, args); // 1, [2,3]
+}
+rest2('1', '2', '3');
+
+//! 튜플, 각자 지켜야함
+const tuple: [string, number] = ['1', 1];
+tuple[2] = 'hello'; // 애는 막는데
+tuple.push('hello'); // 애는 못 막음. 병신임 ㅋㅋ

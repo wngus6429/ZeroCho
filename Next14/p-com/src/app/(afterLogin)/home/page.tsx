9d415455409e1bf73960c9d9dc0a6f74a9aab199
ParkJuhyun <wngus6429@gmail.com>
2024-05-24 01:39:09 +0900

    레이아웃 중앙

--- Next14/p-com/src/app/(afterLogin)/home/page.tsx
+++ Next14/p-com/src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,27 @@
-export default function page() {
-  return <div>홈페이지</div>;
+import PostForm from "./_component/PostForm";
+import Tab from "./_component/Tab";
+import TabProvider from "./_component/TabProvider";
+import style from "./home.module.css";
+
+export default function Home() {
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        {/* <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post /> */}
+      </TabProvider>
+    </main>
+  );
 }

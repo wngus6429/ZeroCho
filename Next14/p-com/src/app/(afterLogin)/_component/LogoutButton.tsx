9227b15bdc15312aeb10156741c153103bd77e51
ParkJuhyun <wngus6429@gmail.com>
2024-07-06 17:53:06 +0900

    Update

--- Next14/p-com/src/app/(afterLogin)/_component/LogoutButton.tsx
+++ Next14/p-com/src/app/(afterLogin)/_component/LogoutButton.tsx
@@ -5,8 +5,8 @@ import style from "./logoutButton.module.css";
 export default function LogoutButton() {
   const me = {
     // 임시로 내 정보 있는것처럼
-    id: "zerohch0",
-    nickname: "제로초",
+    id: "JuhyunPark",
+    nickname: "주현님",
     image: "/5Udwvqim.jpg",
   };
 
--- Next14/p-com/src/mocks/handlers.ts
+++ Next14/p-com/src/mocks/handlers.ts
@@ -11,7 +11,7 @@ function generateDate() {
 }
 const User = [
   { id: "elonmusk", nickname: "Elon Musk", image: "/yRsRRjGO.jpg" },
-  { id: "zerohch0", nickname: "제로초", image: "/5Udwvqim.jpg" },
+  { id: "zerohch0", nickname: "주현님", image: "/5Udwvqim.jpg" },
   { id: "leoturtle", nickname: "레오", image: faker.image.avatar() },
 ];
 const Posts = [];

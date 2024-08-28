231d688d192051e04ff429900388165c95095f97
Park Juhyun <wngus6429@gmail.com>
2024-08-29 03:03:05 +0900

    각종 쿼리

--- Next14/p-com/src/app/(afterLogin)/_component/Trend.tsx
+++ Next14/p-com/src/app/(afterLogin)/_component/Trend.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import style from "./trend.module.css";
-export default function Trend() {
+import { Hashtag } from "@/model/Hashtag";
+
+type Prop = { trend: Hashtag };
+export default function Trend({ trend }: Prop) {
   return (
-    <Link href={`/search?q=트렌드`} className={style.container}>
+    <Link href={`/search?q=${trend.title}`} className={style.container}>
       <div className={style.count}>실시간트렌드</div>
-      <div className={style.title}>제로초</div>
-      <div className={style.count}>1,234 posts</div>
+      <div className={style.title}>{trend.title}</div>
+      {/* toLocaleString으로 3자리 넘어가면 알아서 콤마 , 찍어줌 */}
+      <div className={style.count}>{trend.count.toLocaleString()} posts</div>
     </Link>
   );
 }

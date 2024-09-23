6a1bfa2bd71682efbae32fc47162fe098897f493
ParkJuhyun <wngus6429@gmail.com>
2024-09-24 02:01:49 +0900

    Data cache

--- Next14/p-com/src/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePost.ts
+++ Next14/p-com/src/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePost.ts
@@ -3,7 +3,7 @@ import { Post } from "@/model/Post";
 
 export const getSinglePost: QueryFunction<Post, [_1: string, _2: string]> = async ({ queryKey }) => {
   const [_1, id] = queryKey;
-  const res = await fetch(`http://localhost:9090/api/posts/${id}`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${id}`, {
     next: {
       tags: ["posts", id],
     },
--- Next14/p-com/src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ Next14/p-com/src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -2,11 +2,7 @@ import BackButton from "@/app/(afterLogin)/_component/BackButton";
 import style from "./singlePost.module.css";
 import CommentForm from "@/app/(afterLogin)/[username]/status/[id]/_component/CommentForm";
 import SinglePost from "@/app/(afterLogin)/[username]/status/[id]/_component/SinglePost";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import { getSinglePost } from "@/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePost";
 import { getComments } from "@/app/(afterLogin)/[username]/status/[id]/_lib/getComments";
 import React from "react";
@@ -17,12 +13,8 @@ import { User } from "@/model/User";
 import { Post } from "@/model/Post";
 
 export async function generateMetadata({ params }: Props) {
-  const user: User = await getUserServer({
-    queryKey: ["users", params.username],
-  });
-  const post: Post = await getSinglePostServer({
-    queryKey: ["posts", params.id],
-  });
+  const user: User = await getUserServer({ queryKey: ["users", params.username] });
+  const post: Post = await getSinglePostServer({ queryKey: ["posts", params.id] });
   return {
     title: `Z에서 ${user.nickname} 님 : ${post.content}`,
     description: post.content,
@@ -36,14 +28,8 @@ type Props = {
 export default async function Page({ params }: Props) {
   const { id } = params;
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["posts", id],
-    queryFn: getSinglePost,
-  });
-  await queryClient.prefetchQuery({
-    queryKey: ["posts", id, "comments"],
-    queryFn: getComments,
-  });
+  await queryClient.prefetchQuery({ queryKey: ["posts", id], queryFn: getSinglePostServer });
+  await queryClient.prefetchQuery({ queryKey: ["posts", id, "comments"], queryFn: getComments });
   const dehydratedState = dehydrate(queryClient);
 
   return (

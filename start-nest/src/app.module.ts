4d7dc9ac5483eab216e11ef2b5d4a4362df15d4d
Park Juhyun <wngus6429@gmail.com>
2024-10-30 23:13:47 +0900

    TypeORM 설정

--- start-nest/src/app.module.ts
+++ start-nest/src/app.module.ts
@@ -8,6 +8,8 @@ import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ChannelsModule } from './channels/channels.module';
 import { DmsModule } from './dms/dms.module';
 import { UsersService } from './users/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './entities/Users';
 
 const getEnv = async () => {
   // await axios.get('비밀키요청') 이런식으로도 가능, 외부서버에서 AWS에서 비밀키를 가져옴
@@ -21,11 +23,31 @@ const getEnv = async () => {
   // 이걸로 .env 파일을 읽어올 수 있음
   // imports: [ConfigModule.forRoot({ isGlobal: true, load: [getEnv] })],
   imports: [
-    ConfigModule.forRoot(),
+    // 모든 모듈에서 process.env를 쓸수 있따.
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     UsersModule,
     WorkspacesModule,
     ChannelsModule,
     DmsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      // entities: ['entities/*.ts'], // 이렇게 하면 dist에 있는 파일을 못찾음
+      autoLoadEntities: true, // 자동으로 entities를 로드해줌, 밑에 forFeature인식, 버그 있으면 위에꺼 사용
+      // entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true, // 개발때만 true, 운영에서는 false
+      logging: true, // ORM이 쿼리를 보여줌
+      // 서버가 꺼져도 DB연결을 유지해줌
+      keepConnectionAlive: true, // 자꾸 저장하면 서버 재시작하는데 DB 끊기니까 이걸로 연결 유지
+      charset: 'utf8mb4', // 이모티콘까지 가능
+    }),
+    TypeOrmModule.forFeature([Users]),
   ],
   controllers: [AppController],
   providers: [AppService, UsersService],
---
+++ start-nest/src/entities/Users.ts
@@ -0,0 +1,114 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ChannelChats } from './ChannelChats';
+import { ChannelMembers } from './ChannelMembers';
+import { Channels } from './Channels';
+import { DMs } from './DMs';
+import { Mentions } from './Mentions';
+import { WorkspaceMembers } from './WorkspaceMembers';
+import { Workspaces } from './Workspaces';
+import { ApiProperty } from '@nestjs/swagger';
+
+// Entitiy를 Dto로도 사용가능
+// 유저랑 워크스페이스랑 다대다 관계
+@Index('email', ['email'], { unique: true })
+@Entity({ schema: 'sleact', name: 'users' })
+export class Users {
+  @ApiProperty({
+    example: 1,
+    description: '샤용자 아이디',
+  })
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id: number;
+
+  @ApiProperty({
+    example: 'wngus@gmail.com',
+    description: '이메일',
+  })
+  @Column('varchar', { name: 'email', unique: true, length: 30 })
+  email: string;
+
+  @ApiProperty({
+    example: '주현님',
+    description: '닉네임',
+  })
+  @Column('varchar', { name: 'nickname', length: 30 })
+  nickname: string;
+
+  @Column('varchar', { name: 'password', length: 100, select: false })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+
+  @OneToMany(() => ChannelChats, (channelchats) => channelchats.User)
+  ChannelChats: ChannelChats[];
+
+  @OneToMany(() => ChannelMembers, (channelmembers) => channelmembers.User)
+  ChannelMembers: ChannelMembers[];
+
+  @OneToMany(() => DMs, (dms) => dms.Sender)
+  DMs: DMs[];
+
+  @OneToMany(() => DMs, (dms) => dms.Receiver)
+  DMs2: DMs[];
+
+  @OneToMany(() => Mentions, (mentions) => mentions.Sender)
+  Mentions: Mentions[];
+
+  @OneToMany(() => Mentions, (mentions) => mentions.Receiver)
+  Mentions2: Mentions[];
+
+  @OneToMany(
+    () => WorkspaceMembers,
+    (workspacemembers) => workspacemembers.User,
+  )
+  WorkspaceMembers: WorkspaceMembers[];
+
+  @OneToMany(() => Workspaces, (workspaces) => workspaces.Owner)
+  OwnedWorkspaces: Workspaces[];
+
+  @ManyToMany(() => Workspaces, (workspaces) => workspaces.Members)
+  @JoinTable({
+    name: 'workspacemembers',
+    joinColumn: {
+      name: 'UserId',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'WorkspaceId',
+      referencedColumnName: 'id',
+    },
+  })
+  Workspaces: Workspaces[];
+
+  @ManyToMany(() => Channels, (channels) => channels.Members)
+  @JoinTable({
+    name: 'channelmembers',
+    joinColumn: {
+      name: 'UserId',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'ChannelId',
+      referencedColumnName: 'id',
+    },
+  })
+  Channels: Channels[];
+}

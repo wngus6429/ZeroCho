90889bf70eff20013fde83503424f5894fec9252
Park Juhyun <wngus6429@gmail.com>
2024-12-12 01:46:26 +0900

    f

--- Next14/p-com/src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
+++ Next14/p-com/src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
@@ -6,7 +6,7 @@ import LoginModal from "@/app/(beforeLogin)/_component/LoginModal";
 export default function Page() {
   return (
     <>
-      난 가로채기지롱 ㅋㅋ
+      @modal에로그인쪽컴포넌트
       <LoginModal />
     </>
   );
--- Next14/p-com/src/app/(beforeLogin)/page.tsx
+++ Next14/p-com/src/app/(beforeLogin)/page.tsx
@@ -1,6 +1,5 @@
 // import styles from "@/app/page.module.css"; //! module.css를 통해서 다른 파일이랑 구별을 해준다
 //! 특정한 페이지나 레이아웃에 적용
-// tailwind 호불호 많이 심함
 // styled component -> Server Compoentn SSR 문제 있음.
 // sass,
 // cssmodule 간단하다

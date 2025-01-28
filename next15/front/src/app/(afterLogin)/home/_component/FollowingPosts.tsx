6f5e89281304db45adcf0616e258d45f954f5a4f
Park Juhyun <wngus6429@gmail.com>
2025-01-28 23:21:46 +0900

    Update FollowingPosts.tsx

--- next15/front/src/app/(afterLogin)/home/_component/FollowingPosts.tsx
+++ next15/front/src/app/(afterLogin)/home/_component/FollowingPosts.tsx
@@ -1,15 +1,19 @@
 "use client";
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { getFollowingPosts } from "@/app/(afterLogin)/home/_lib/getFollowingPosts";
 import Post from "@/app/(afterLogin)/_component/Post";
 import { Post as IPost } from "@/model/Post";
 
 export default function FollowingPosts() {
-  const { data } = useQuery<IPost[]>({
+  // 상위에 Suspense를 인식하고, home/page에 있는 TabDeciderSuspense에서 prefetchInfiniteQuery를 통해 데이터를 미리 가져옴
+  const { data, isPending } = useSuspenseQuery<IPost[]>({
     queryKey: ["posts", "followings"],
     queryFn: getFollowingPosts,
     staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
     gcTime: 300 * 1000,
   });
+
+  // 위에 useSuspenseQuery로 인해 상위 fallback이 실행되면서 로딩화면이 나옴
+
   return data?.map((post) => <Post key={post.postId} post={post} />);
 }

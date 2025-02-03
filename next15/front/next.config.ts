32df7b989174224e24393f085c3fef3337536376
Park Juhyun <wngus6429@gmail.com>
2025-02-04 02:55:47 +0900

    Update next.config.ts

--- next15/front/next.config.ts
+++ next15/front/next.config.ts
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
+  },
   async rewrites() {
     return [
       {
-        source: "/upload/:slug",
+        source: "/upload/:slug", // 이런 파일 만나면, 아래와 같이 붙여라
         destination: `${process.env.NEXT_PUBLIC_BASE_URL}/upload/:slug`, // Matched parameters can be used in the destination
       },
     ];
